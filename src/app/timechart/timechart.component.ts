9e60c393f2c5e823a52d3e85dc8b2c228f6f021a
Temich Koziar <a@temich.in.ua>
2018-05-08 20:27:17 +0300

    hours axises

--- src/app/timechart/timechart.component.ts
+++ src/app/timechart/timechart.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-timechart',
   templateUrl: './timechart.component.html',
-  styleUrls: ['./timechart.component.css']
+  styleUrls: ['./timechart.component.scss']
 })
 export class TimechartComponent implements OnInit {
+
+  public hours = [5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4];
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.hours);
   }
 
 }
